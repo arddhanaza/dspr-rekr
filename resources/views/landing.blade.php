ee0c0b71282bf350f1b412d0085d1e395975acb6
Arddhana Zhafran Amanullah <arddhanaza99@gmail.com>
2021-06-04 08:18:16 +0700

    update_css

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('assets/scss/bootstrap.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/dist/css/style.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('assets/scss/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('assets/dist/css/style.css')}}">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
     <title>Login</title>
 
